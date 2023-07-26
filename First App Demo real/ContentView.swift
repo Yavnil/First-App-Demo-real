c003bd42b2e004ea999164553124d44b81577c36
Yavnil <2012754@apps.nsd.org>
2023-07-26 10:35:26 -0700

    Initial Commit

---
+++ First App Demo real/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  First App Demo real
+//
+//  Created by Al Yavnilovitch on 7/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First App Demo real/First_App_Demo_realApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_App_Demo_realApp.swift
+//  First App Demo real
+//
+//  Created by Al Yavnilovitch on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct First_App_Demo_realApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
